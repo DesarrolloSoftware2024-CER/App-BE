ff995bedc8ce08850a612949bd0d8c8b71f5f4c8
valeriaegel <86688855+valeriaegel@users.noreply.github.com>
2024-09-11 22:17:44 -0300

    dto

---
+++ src/Watchly.Application.Contracts/Series/SerieDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace Watchly.Series
+{
+    public class SerieDTO: EntityDto<int>
+    {
+        public string Title { get; set; }
+    }
+}
